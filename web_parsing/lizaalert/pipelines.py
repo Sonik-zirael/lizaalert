7f62e08665a0a2cd3db500f82bffa7a45bfe983e
Sophia <sofiya_shalgueva@mail.ru>
2021-10-27 23:10:02 +0300

    Download all forum as json

---
+++ web_parsing/lizaalert/pipelines.py
@@ -0,0 +1,22 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+import json
+
+
+class LizaalertPipeline:
+    def process_item(self, item, spider):
+        spider.counter += 1
+        print(f'processing link {spider.counter:07d}. Total links: {len(spider.all_visited_links_set):07d}', end='\r')
+        if spider.counter % 500 == 0:
+            print()
+        return item
+
+    def close_spider(self, spider):
+        with open("result.json", "w", encoding='utf8') as o_f:
+            json.dump(spider.results_by_region, o_f, ensure_ascii=False, indent=4)

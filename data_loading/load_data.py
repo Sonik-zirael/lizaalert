79e67986775e5f3c57e19883961a56c867bc037f
Sophia <sofiya_shalgueva@mail.ru>
2021-12-17 21:55:46 +0300

    Dome fixes

--- data_loading/load_data.py
+++ data_loading/load_data.py
@@ -1,10 +1,11 @@
 import json
 from json.decoder import JSONDecodeError
 import logging
+import random
 import ssl
 import zipfile
 import argparse
-from datetime import datetime
+from datetime import datetime, time
 
 import certifi
 import geopy.geocoders
@@ -87,77 +88,63 @@ if (args.mode) == 'archive':
 else:
     print('Work with kafka')
     indices = ['pipeline']
+
     if es.indices.exists(index=indices):
         es.delete_by_query(index=indices, body={"query": {"match_all": {}}})    # this will clean all data in db
         es.indices.delete(index=indices)    # this will drop index
     es.indices.create(index=indices, body=mappingsElastic)  # this will create index with mapping. Nesseccery to store coordinates as geo_point
-    topic_name = "parsed_data_1"
-    consumer = KafkaConsumer(topic_name, auto_offset_reset='earliest',
-                             bootstrap_servers=['kafka:9092'], api_version=(0, 10), consumer_timeout_ms=1000,
-                             group_id='load_data_group')
-    is_end_flag = False
-    iterations_without_change = 0
+    # random_number = random.randint(280, 320)
+    # time.sleep(random_number)
+
+    parsed_data_zip = zipfile.ZipFile(r"../parsed.zip", "r")
+
+    topics = None
+    with parsed_data_zip.open("data.json", "r") as read_file:
+        topics = json.loads(read_file.read().decode('utf-8'))
+
     regions_coords = {}
-    while True:
-        iterations_without_change += 1
-        if is_end_flag:
-            break
-        for message in consumer:
-            consumer.commit()
-            iterations_without_change = 0
-            message_string = message.value.decode('utf-8')
-            try:
-                message_data = json.loads(message_string)
-                is_end_flag = (message_data == 'end')
-                if is_end_flag:
-                    break
-            except JSONDecodeError as jde:
-                logging.warning('Received message %s in topic %s which is not in json format',
-                                message_string, topic_name)
-            message_data = message_data[0]
-            geolocator = Nominatim(scheme='http', user_agent="lizaalert", timeout = 100)
-            locations = message_data["Location"]
-            newLocation = None
+    for topic in topics:
+        random_number = random.randint(7, 15)
+        time.sleep(random_number)
+        geolocator = Nominatim(scheme='http', user_agent="lizaalert", timeout = 100)
+        locations = topic["Location"]
+        newLocation = None
+        for location in locations:
+            if location["value"]["type"] in ["область", "регион", "республика", "край", "автономный округ"]:
+                newLocation = location["value"]["name"] + " " + location["value"]["type"]
+        if newLocation is None:
             for location in locations:
-                if location["value"]["type"] in ["область", "регион", "республика", "край", "автономный округ"]:
-                    newLocation = location["value"]["name"] + " " + location["value"]["type"]
-            if newLocation is None:
-                for location in locations:
-                    if location["value"]["type"] in ["село", "город", "г.", "поселок", "п."]:
-                        newLocation = location["value"]["name"]
+                if location["value"]["type"] in ["село", "город", "г.", "поселок", "п."]:
+                    newLocation = location["value"]["name"]
 
-            if newLocation not in regions_coords:
-                if newLocation is not None:
-                    coordinates = geolocator.geocode(newLocation)
-                    region = None
-                    if coordinates is not None:
-                        locationList = coordinates.address.split(', ')
-                        if locationList[-1] == "Россия":
-                            for i in range(len(locationList) - 1, 0, -1):
-                                if "федеральный округ" in locationList[i]:
-                                    region = locationList[i - 1]
-                                    break
-                        elif locationList[-1] in ["Беларусь", "Україна", "Қазақстан"]:
-                            region = newLocation + ", " + locationList[-1]
-                            for i in range(len(locationList) - 1, 0, -1):
-                                if "область" in locationList[i] or "район" in locationList[i] or "округ" in locationList[i]:
-                                    region = locationList[i] + ", " + locationList[-1]
-                                    break
-                        regions_coords[newLocation] = {
-                            "region": region,
-                            "LocationCoordinates": f"{coordinates.latitude},{coordinates.longitude}"
-                        }
-            if regions_coords.get(newLocation) is not None:
-                message_data["LocationCoordinates"] = regions_coords[newLocation]["LocationCoordinates"]
-                message_data["ShortLocation"] = regions_coords[newLocation]["region"]
-                print(regions_coords[newLocation]["region"])
-            es.index(
-                index=indices,
-                document=message_data
-            )
-            print(message_data)
-
-    consumer.commit()
-    consumer.close()
+        if newLocation not in regions_coords:
+            if newLocation is not None:
+                coordinates = geolocator.geocode(newLocation)
+                region = None
+                if coordinates is not None:
+                    locationList = coordinates.address.split(', ')
+                    if locationList[-1] == "Россия":
+                        for i in range(len(locationList) - 1, 0, -1):
+                            if "федеральный округ" in locationList[i]:
+                                region = locationList[i - 1]
+                                break
+                    elif locationList[-1] in ["Беларусь", "Україна", "Қазақстан"]:
+                        region = newLocation + ", " + locationList[-1]
+                        for i in range(len(locationList) - 1, 0, -1):
+                            if "область" in locationList[i] or "район" in locationList[i] or "округ" in locationList[i]:
+                                region = locationList[i] + ", " + locationList[-1]
+                                break
+                    regions_coords[newLocation] = {
+                        "region": region,
+                        "LocationCoordinates": f"{coordinates.latitude},{coordinates.longitude}"
+                    }
+        if regions_coords.get(newLocation) is not None:
+            topic["LocationCoordinates"] = regions_coords[newLocation]["LocationCoordinates"]
+            topic["ShortLocation"] = regions_coords[newLocation]["region"]
+            print(regions_coords[newLocation]["region"])
+        es.index(
+            index=indices,
+            document=topic
+        )
 
 print('Done data loading')
